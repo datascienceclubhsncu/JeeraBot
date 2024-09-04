2b160aa2a8dbd18b782768f8e9cf1224aefdcc12
Vinay Gajula <glucose048@gmail.com>
2024-09-04 14:54:15 +0530

    llamaparse integartion

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JeeraBot
 Chatbot for SAS
+
+commands to run llamaparse
+
+pip install -r requirements.txt
+
+cd llamaparse
+
+python parse.py
---
+++ llamaparse/parse.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index.llms.groq import Groq
+from llama_index.core import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+#set the embedding settings
+Settings.embed_model= HuggingFaceEmbedding(
+    model_name="Alibaba-NLP/gte-multilingual-base",
+    trust_remote_code=True
+)
+
+#set up a parser
+parser= LlamaParse(
+    result_type='markdown'
+)
+
+llm= Groq(
+    model="llama3-70b-8192"
+)
+
+file_extractor={'.pdf': parser}
+
+sub_doc=SimpleDirectoryReader(input_dir='data', file_extractor=file_extractor)
+documents= sub_doc.load_data()
+
+index= VectorStoreIndex.from_documents(documents=documents)
+
+# create a query engine for the index
+query_engine = index.as_query_engine(
+    llm=llm,
+)
+
+while (prompt := input("Ask anything? ")) !='q':
+    response= query_engine.query(prompt)
+    print(response)
