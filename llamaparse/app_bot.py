5cf56dbe3883937827ff4aa6e74d9da0dc19f575
Research_Cell_SAS <99862955+datascienceclubhsncu@users.noreply.github.com>
2024-11-26 22:23:14 +0530

    Update app_bot.py

--- llamaparse/app_bot.py
+++ llamaparse/app_bot.py
@@ -77,10 +77,10 @@ col1, col3 = st.columns([1, 1])
 
 # Display logos with mobile settings (use_column_width=True)
 with col1:
-    st.image("llamaparse/data/Logo_HSNC.png", use_column_width=True)
+    st.image("llamaparse/data/Logo_HSNC.png", width=100)
 
 with col3:
-    st.image("llamaparse/data/Logo_SAS.png", use_column_width=True)
+    st.image("llamaparse/data/Logo_SAS.png", width=100)
 
 # Title in the center
 st.markdown(
